e3cc59ee5b77b96c5b97d6503aba7af005709348
Taha Nadeem Khan <tahak6884@gmail.com>
2026-02-12 00:18:47 +0500

    my first commit

---
+++ agent.py
@@ -0,0 +1,24 @@
+from agents import Agent, Runner, function_tool, OpenAIChatCompletionsModel
+from openai import AsyncOpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+
+provider = AsyncOpenAI(api_key=api_key, base_url="https://generativelanguage.googleapis.com/v1beta/openai/") 
+
+
+model = OpenAIChatCompletionsModel(model="gemini-2.5-flash", openai_client=provider)
+
+
+scraping_agent = Agent(
+    name="ScraperAgent",
+    instructions="You are an expert data extraction agent. Extract school events and fees from the provided text.",
+    model=model
+)
+
+result = Runner.run_sync(scraping_agent, input="")
+print(result.final_output)
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from axiom-experts!")
+
+
+if __name__ == "__main__":
+    main()
